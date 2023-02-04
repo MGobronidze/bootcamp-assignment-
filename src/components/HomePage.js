9d1eaf339df35039c0f7181610bcdd0f87dc85ae
MGobronidze <90101429+MGobronidze@users.noreply.github.com>
2023-02-04 15:57:33 +0400

    commiting home page

---
+++ src/components/HomePage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import background from "./images/background-image.png";
+import logo from "./images/LOGO.png"
+import redberryLogo from "./images/redberry-logo.png"
+
+export default function HomePage(){
+    return(
+        <div className="home-page" style={{ backgroundImage:`url(${background})`}}>
+            <img src={redberryLogo} className="redberry-logo" alt="here is logo"></img>
+            <div className="line"></div>
+            <button className="home-button">რეზიუმეს დამატება</button>
+            <img src={logo} className="home-page-logo" alt="here is logo"></img>
+        </div>
+    )
+}
