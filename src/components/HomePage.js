13e30d8a455e5c23f125d1aebab7ab2aa7f4dc1f
MGobronidze <90101429+MGobronidze@users.noreply.github.com>
2023-02-06 20:04:46 +0400

    adding reuters

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,16 +1,29 @@
 import React from "react";
 import background from "./images/background-image.png";
-import logo from "./images/LOGO.png"
-import redberryLogo from "./images/redberry-logo.png"
-import "./homepage.css"
+import logo from "./images/LOGO.png";
+import redberryLogo from "./images/redberry-logo.png";
+import "./homepage.css";
+import {BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
+import PriviteInfo from './PriviteInfo';
 
 export default function HomePage(){
+
+    
     return(
         <div className="container" style={{ backgroundImage:`url(${background})`}}>
             <img src={redberryLogo} className="redberry-logo" alt="here is logo"></img>
             <div className="line"></div>
-            <button className="home-button">რეზიუმეს დამატება</button>
-            <img src={logo} className="home-page-logo" alt="here is logo"></img>
+            <Router>
+                <Link to="/PriviteInfo">
+                    <button className="home-button">რეზიუმეს დამატება</button>
+                </Link>
+            
+                <img src={logo} className="home-page-logo" alt="here is logo"></img>
+
+                <Routes>
+                        <Route path="/PriviteInfo"> element ={<PriviteInfo />}</Route>
+                </Routes> 
+            </Router>
         </div>
     )
 }
